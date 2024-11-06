0f811ed322fd5a30a5bbefb160549f16da1596f4
Jimmy Nguyen <dtstego@gmail.com>
2024-11-06 10:36:33 -0800

    Adds UserContext to login page (index.tsx).
    Creates a global context so the current user's information can be passed to other pages.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,91 +1,76 @@
-import { Image, StyleSheet, View, Button, Text } from 'react-native';
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import React, { useState, useEffect } from 'react';
-import { HelloWave } from '@/components/HelloWave';
+import { Image, StyleSheet, View, Button } from 'react-native';
+import React, { useEffect } from 'react';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Link, router } from 'expo-router';
 
-//Auth
+// Authentication imports.
 import * as Google from "expo-auth-session/providers/google";
-import * as WebBrowser from "expo-web-browser";
-import {
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithCredential,
-} from "firebase/auth"
-import { auth } from "../firebaseConfig";
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import { GoogleAuthProvider, signInWithCredential, } from "firebase/auth"
+import { auth } from "@/firebaseConfig";
+
+import { useUser } from "./UserContext";
 
 export default function App() {
-
-  const [userInfo, setUserInfo] = React.useState();
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId:process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
-    webClientId:process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
-    redirectUri:"com.tasty.tastytrade:/(onboarding)/start"
-  });
-  console.log(request?.redirectUri);
-
-  // Function to handle login submission
-  React.useEffect(() => {
-    if (response?.type == "success") {
-      console.log("Success!, signing in");
-      const {id_token} = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
-      signInWithCredential(auth, credential)
-    }
-  }, [response]);
-  
-  React.useEffect(() => {
-    const unsub = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        console.log(JSON.stringify(user, null, 2));
-        setUserInfo(user);
-        await AsyncStorage.setItem("@user", JSON.stringify(user));
-      } else {
-        console.log("User is not authenticated");
-      }
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+        webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+        redirectUri: "com.tasty.tastytrade:/(onboarding)/start"
     });
-  
-    return () => unsub();
-  }, []);
 
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: 'rgba(255, 255, 255, 0.5)', dark: 'rgba(0, 0, 0, 0)' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/tasty-trade.jpg')}
-          style={styles.reactLogo}
-        />
-      }>
-      <View style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! to TastyTrade</ThemedText>
-      </View>
-      <View style={styles.titleContainer}>
-        {/* {error && <Text style={styles.errorText}>{error}</Text>} */}
-        <Button title="Sign In with Google" onPress={() => promptAsync()} />
-      </View>
-    </ParallaxScrollView>
-  );
-}
+    // Hook from UserContext to set the current, global user for the application.
+    const { setUser, user } = useUser();
+
+    // Function to handle login submission.
+    useEffect(() => {
+        if (response?.type == "success") {
+            const { id_token } = response.params;
+            const credential = GoogleAuthProvider.credential(id_token);
+            signInWithCredential(auth, credential)
+                .then((result) => {
+                    // Sets current user to UserContext.
+                    setUser({email: result.user.email, name: result.user.displayName, uid: result.user.uid});
+                    // TODO: Register user to firebase DB if it doesn't exist.
+                })
+                .catch((error) => {
+                    console.error("Fatal error signing in: ", error);
+                });
+        }
+    }, [response]);
+
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{light: 'rgba(255, 255, 255, 0.5)', dark: 'rgba(0, 0, 0, 0)'}}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/tasty-trade.jpg')}
+                    style={styles.reactLogo}
+                />
+            }>
+            <View style={styles.titleContainer}>
+                <ThemedText type="title">Welcome! to TastyTrade</ThemedText>
+            </View>
+            <View style={styles.titleContainer}>
+                {/* {error && <Text style={styles.errorText}>{error}</Text>} */}
+                <Button title="Sign In with Google" onPress={() => promptAsync()}/>
+            </View>
+        </ParallaxScrollView>
+    );
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    alignSelf: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  reactLogo: {
-    height: 200,
-    width: 200,
-    alignSelf: 'center',
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 10,
-  },
+    titleContainer: {
+        alignSelf: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    reactLogo: {
+        height: 200,
+        width: 200,
+        alignSelf: 'center',
+    },
+    errorText: {
+        color: 'red',
+        marginBottom: 10,
+    },
 });
